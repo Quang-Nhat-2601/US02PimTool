e66d4f9502915380626ea04a8573bed524c63eb4
Quang-Nhat-2601 <quangnhatle2601@gmail.com>
2024-08-27 13:49:15 +0700

    85%

--- elca-project-form/src/index.ts
+++ elca-project-form/src/index.ts
@@ -1,2 +1,24 @@
 import './style.scss';
+import $ from 'jquery';
 
+$(function () {
+    $("#project-list-link").on('click', () => {
+        let pageInput:Xrm.Navigation.PageInputHtmlWebResource = {
+            pageType: "webresource",
+            webresourceName: "elca_projectmanagementpage"
+        };
+
+        var navigationOptions :Xrm.Navigation.NavigationOptions= {
+            target: 1,
+        };
+
+        parent.Xrm.Navigation.navigateTo(pageInput, navigationOptions).then(
+            () => {
+                console.log("Navigatied");
+            },
+            (error) => {
+                console.log("Navigate failed {0}", error);
+            }
+        );
+    });
+})
